c01f49a11e054914eb0d362d104e4f8ccce02e34
mohamed-azikiou <50836748+mohamed-azikiou@users.noreply.github.com>
2019-06-13 07:08:07 +0100

    Add files via upload

--- Projet Documents Structurées/OpenCmdHere.bat
+++ Projet Documents Structurées/OpenCmdHere.bat
@@ -2,7 +2,7 @@
 echo             _____________________________________________________________________________________________
 echo            /                                                                                            /
 echo           /  Utilisation: Execute.jar Chemin_Vers_Le_Dossier_Soucre                                    /
-echo          /  exemple: Execute.jar examen                                                               /
+echo          /  exemple: Execute.jar examen (Sinon java -jar Execute.jar examen)                          /
 echo         /  A noter que le chemin ne doit pas contenir des espaces sauf s'il est entoure par des (")  /
 echo        /  exemple: Execute.jar "chemin vers source"                                                 /
 echo       /  Le Fichier "Preprocess.py" doit etre dans le meme niveau que "Execute.jar"                /
